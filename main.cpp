1e6bdad049a49f3b9401e7de2050bbad49468e9c
Igor <jaknikig@mail.ru>
2023-07-05 13:26:26 +0600

    fermi

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+int fermi(int guess_number, int usr_number){
+    int count = 0;
+    while (guess_number > 0) {
+        if ((guess_number % 10) == (usr_number % 10)) {
+            count++;
+        }
+        guess_number = guess_number / 10;
+        usr_number = usr_number / 10;
+    }
+    return count;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int guess_number = 123;
+    int user_number = 423;
+    std::cout << "Хорошо, пора начинать. Я загадал число. Удачи!" << std::endl;
+
+    std::cout << "Попытка №" << "\n"; //добавить номер попытки
+    //std::cin >> user_number;
+    std::cout << "Совпадений = " << fermi(guess_number, user_number) << "\n";
     return 0;
 }
