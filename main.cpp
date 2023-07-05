c6e74fd7e8daeadef2a0d6efddf0bdb1ed8be569
Igor <jaknikig@mail.ru>
2023-07-05 15:12:59 +0600

    pico and ferme

--- main.cpp
+++ main.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 
-int fermi(int guess_number, int usr_number){
+int fermi(std::string guess_number, std::string usr_number, int number_of_digit){
     int count = 0;
-    while (guess_number > 0) {
-        if ((guess_number % 10) == (usr_number % 10)) {
+    for(int i = 0; i < number_of_digit; i++) {
+        if (guess_number[i] == usr_number[i]) {
             count++;
         }
-        guess_number = guess_number / 10;
-        usr_number = usr_number / 10;
     }
     return count;
 }
 
-int pico(int guess_number, int usr_number, int* arr_size){
+int pico(std::string guess_number, std::string usr_number, int number_of_digits){
     int count = 0;
-    int array[*arr_size];
-    for(int i = 0; i < *arr_size; i++){
-        array[i] = guess_number % 10;
-        guess_number /= 10;
-    }
 
-    for(int i = 0; i < *arr_size; i++){
-        for(int k = 0; k < *arr_size; k++){
-            if(array[i] == usr_number % 10){
+    for(int i = 0; i < number_of_digits; i++){
+        for(int k = 0; k < number_of_digits; k++){
+
+            if(usr_number[k] == guess_number[i]){
                 count++;
-            }
-            usr_number /= 10;
+                usr_number[k] = '-';
+                guess_number[i] = '+';
+                k = number_of_digits;
         }
+    }
     }
     return count;
 }
 
 int main() {
-    int guess_number = 123;
-    int user_number = 423;
-    int arr_size;
+    std::string guess_number = "14501";
+    std::string user_number =  "14015";
+    int number_of_digit = 5;
+    int fermi_number;
+    int pico_number;
     std::cout << "Хорошо, пора начинать. Я загадал число. Удачи!" << std::endl;
 
     std::cout << "Попытка №" << "\n"; //добавить номер попытки
     //std::cin >> user_number;
-    std::cout << "Совпадений = " << fermi(guess_number, user_number) << "\n";
+    fermi_number = fermi(guess_number, user_number, number_of_digit);
+    pico_number = pico(guess_number, user_number,number_of_digit);
+    std::cout << "Совпадений FERME = " << fermi_number << "\n";
+    std::cout << "Совпадений ПИКО = " << pico_number - fermi_number << "\n";
+    //pico( guess_number, user_number,arr_size);
     return 0;
 }
