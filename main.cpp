a298753f10fbf38cbb816a05a0ce5bb577f313ed
Igor <jaknikig@mail.ru>
2023-07-05 14:00:37 +0600

    pico with array

--- main.cpp
+++ main.cpp
@@ -12,9 +12,29 @@ int fermi(int guess_number, int usr_number){
     return count;
 }
 
+int pico(int guess_number, int usr_number, int* arr_size){
+    int count = 0;
+    int array[*arr_size];
+    for(int i = 0; i < *arr_size; i++){
+        array[i] = guess_number % 10;
+        guess_number /= 10;
+    }
+
+    for(int i = 0; i < *arr_size; i++){
+        for(int k = 0; k < *arr_size; k++){
+            if(array[i] == usr_number % 10){
+                count++;
+            }
+            usr_number /= 10;
+        }
+    }
+    return count;
+}
+
 int main() {
     int guess_number = 123;
     int user_number = 423;
+    int arr_size;
     std::cout << "Хорошо, пора начинать. Я загадал число. Удачи!" << std::endl;
 
     std::cout << "Попытка №" << "\n"; //добавить номер попытки
