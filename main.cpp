a8cfb6b071ace5bbef870ad8ef5027508c285548
Igor <jaknikig@mail.ru>
2023-07-05 23:23:38 +0600

    added output 5 last numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 int fermi(std::string guess_number, std::string usr_number, int number_of_digit){
     int count = 0;
@@ -30,26 +31,37 @@ int pico(std::string guess_number, std::string usr_number, int number_of_digits)
 
 bool input_usr(std::string input, int number_of_digit){
     //std::cout << atoi(input);
-
-    if(atoi(input) < pow(10, number_of_digit - 1)) return false ;
-    return true;
+    if((input.size() == number_of_digit) && (all_of(input.begin(), input.end(), ::isdigit))){
+        return true;
+    }
+    //std::cout << std::all_of(input.begin(), input.end(), ::isdigit);
+    //if(atoi(input) < pow(10, number_of_digit - 1)) return false ;
+    return false;
 } //проверка на правильность ввода
 
 int main() {
-    std::string guess_number = "545";
+    std::string guess_number = "12345";
     std::string user_number = "";
     int number_of_digit;
     int fermi_number;
     int pico_number;
+    //int count_array = 0;
+    std::string* save_number = new std::string [20];
     std::cout << "Какое количество цифр желаете отгадывать? ";
     std::cin >> number_of_digit;
     std::cout << "Хорошо, пора начинать. Я загадал число. Удачи!" << std::endl;
 
     for(int i = 1; i < 21; i++){
+        int a = 0;
+        if(i > 6) a = i - 6;
+        for(int l = a; l < i; l++){
+            std::cout << "            " << save_number[l] << "\n";
+        }
         std::cout << "Попытка №" << i << "  ";
         std::cin >> user_number;
+        save_number[i - 1] = user_number;//print
         if(!input_usr(user_number,number_of_digit)){
-            std::cout << "Неправильный ввод, попробуйте заново.";
+            std::cout << "Неправильный ввод, попробуйте заново. \n";
             i = i - 1;
             continue;
         }else{
@@ -58,18 +70,18 @@ int main() {
             std::cout << "В цель = " << fermi_number << "\n";
             std::cout << "Рядом = " << pico_number - fermi_number << "\n";
         }
-        std::cout << "input = " << user_number << "\n";
+
 
     }
     //std::cout << "Попытка №" << "\n"; //добавить номер попытки
     //std::cin >> user_number;
-    fermi_number = fermi(guess_number, user_number, number_of_digit);
-    pico_number = pico(guess_number, user_number,number_of_digit);
+    //fermi_number = fermi(guess_number, user_number, number_of_digit);
+    //pico_number = pico(guess_number, user_number,number_of_digit);
    // std::cout << "Совпадений FERME = " << fermi_number << "\n";
     //std::cout << "Совпадений ПИКО = " << pico_number - fermi_number << "\n";
     //pico( guess_number, user_number,arr_size);
     //std::cout << input_usr(guess_number,number_of_digit) << "\n";
     //std::cout << std::atoi(guess_number) << "\n" << pow(10, 5);
-
+    delete[] save_number;
     return 0;
 }
