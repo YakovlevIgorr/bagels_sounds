c6a0ccc438e79cd789a1aa90b93c7a2cf3f5ab6f
Igor <jaknikig@mail.ru>
2023-07-07 00:04:39 +0600

    randon and access to input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
+//#include <vector>
+#include <ctime>
 
 int fermi(std::string guess_number, std::string usr_number, int number_of_digit){
     int count = 0;
@@ -30,25 +31,48 @@ int pico(std::string guess_number, std::string usr_number, int number_of_digits)
 }
 
 bool input_usr(std::string input, int number_of_digit){
-    //std::cout << atoi(input);
     if((input.size() == number_of_digit) && (all_of(input.begin(), input.end(), ::isdigit))){
         return true;
     }
-    //std::cout << std::all_of(input.begin(), input.end(), ::isdigit);
-    //if(atoi(input) < pow(10, number_of_digit - 1)) return false ;
     return false;
 } //проверка на правильность ввода
 
+std::string random_guess(int number_of_digit){//3
+    srand(time(nullptr));
+    number_of_digit = pow(10,number_of_digit);
+    return std::to_string (rand() % number_of_digit);
+
+}// генерирует число
+
 int main() {
-    std::string guess_number = "12345";
+    std::string guess_number = "";
     std::string user_number = "";
     int number_of_digit;
+    std::string string_number_of_digit = "";
     int fermi_number;
     int pico_number;
     //int count_array = 0;
     std::string* save_number = new std::string [20];
-    std::cout << "Какое количество цифр желаете отгадывать? ";
-    std::cin >> number_of_digit;
+    while (true) {
+        std::cout << "Какое количество цифр желаете отгадывать? Не более 9. ";
+        std::cin >> string_number_of_digit;
+
+        if (all_of(string_number_of_digit.begin(),string_number_of_digit.end(),::isdigit)){
+            number_of_digit = std::stoi(string_number_of_digit);
+        } else {
+            std::cout << "Попробуйте еще раз.\n";
+            continue;
+        }
+
+        if (number_of_digit > 0 && number_of_digit < 10) {
+            guess_number = random_guess(number_of_digit);
+            std::cout << "guess number = " << guess_number << "\n";//after clear
+            break;
+        } else{
+            std::cout << "Попробуйте еще раз.\n";
+        }
+    }
+
     std::cout << "Хорошо, пора начинать. Я загадал число. Удачи!" << std::endl;
 
     for(int i = 1; i < 21; i++){
@@ -69,6 +93,10 @@ int main() {
             pico_number = pico(guess_number, user_number,number_of_digit);
             std::cout << "В цель = " << fermi_number << "\n";
             std::cout << "Рядом = " << pico_number - fermi_number << "\n";
+        }
+        if(fermi_number == number_of_digit){
+            std::cout << "Победа! Число угадано верно.\n";
+            break;
         }
 
 
